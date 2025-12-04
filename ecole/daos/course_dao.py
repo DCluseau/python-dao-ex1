97cb98d409ae85d6c9dddecd8792320c39b24693
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-12-04 14:56:11 +0100

    Added getting teachers methods

--- ecole/daos/course_dao.py
+++ ecole/daos/course_dao.py
@@ -9,6 +9,8 @@ from daos.dao import Dao
 from dataclasses import dataclass, field
 from typing import Optional
 
+from models.teacher import Teacher
+
 
 @dataclass
 class CourseDao(Dao[Course]):
@@ -49,10 +51,41 @@ class CourseDao(Dao[Course]):
             for course in record:
                 course_one = Course(course['name'], course['start_date'], course['end_date'])
                 course_one.id = course['id_course']
+                course_one.teacher = CourseDao().get_teacher(course_one.id)
                 courses.append(course_one)
         else:
-            course = None
+            courses = None
         return courses
+
+    @staticmethod
+    def get_teacher(id_course):
+        """Renvoie le cours correspondant à l'entité dont l'id est id_course
+           (ou None s'il n'a pu être trouvé)"""
+
+        with Dao.connection.cursor() as cursor:
+            sql = "SELECT id_teacher FROM course WHERE id_course=%s"
+            cursor.execute(sql, (id_course,))
+            record = cursor.fetchone()
+        if record is not None:
+            with Dao.connection.cursor() as cursor:
+                id_teacher = record['id_teacher']
+                sql = "SELECT hiring_date FROM teacher WHERE id_teacher=%s"
+                cursor.execute(sql, id_teacher)
+                record = cursor.fetchone()
+                if record is not None:
+                    hiring_date = record['hiring_date']
+                else:
+                    hiring_date = ""
+                # Créer un objet Teacher
+                sql = "SELECT teacher.id_teacher, person.first_name, person.last_name, person.age, address.street, address.city, address.postal_code FROM person LEFT OUTER JOIN address ON person.id_address = address.id_address LEFT OUTER JOIN teacher ON teacher.id_person = person.id_person WHERE teacher.id_teacher=%s"
+                cursor.execute(sql, (id_teacher,))
+                record = cursor.fetchone()
+                if record is not None:
+                    teacher = Teacher(record['first_name'], record['last_name'], record['age'], hiring_date)
+
+        else:
+            teacher = None
+        return teacher
 
     def update(self, course: Course) -> bool:
         """Met à jour en BD l'entité Course correspondant à course, pour y correspondre
