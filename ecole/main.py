e9a96f97d0dc669571fa7f3af35cb98e1927e895
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-12-05 16:01:42 +0100

    Test address functions (to correct)

--- ecole/main.py
+++ ecole/main.py
@@ -6,6 +6,7 @@ Application de gestion d'une école
 """
 
 from business.school import School
+from models.address import Address
 
 
 def main() -> None:
@@ -27,7 +28,14 @@ Bienvenue dans notre école
     print(school.get_course_by_id(1))
     print(school.get_course_by_id(2))
     print(school.get_course_by_id(9))
-
+    new_address_street = "33 rue des péniches"
+    if school.courses[1].teacher.address is not None:
+        # A mettre dans school --> se baser sur add_courses
+        school.courses[1].teacher.address.set(new_address_street, school.courses[1].teacher.address.city,
+                                          school.courses[1].teacher.address.postal_code)
+    #else:
+    #    school.courses[1].teacher.address = Address(new_address_street, "",
+    #     "")
 
 if __name__ == '__main__':
     main()
